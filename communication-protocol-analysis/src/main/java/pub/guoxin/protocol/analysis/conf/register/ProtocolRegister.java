685e9829fbde057ab3b3f787a3788d0e2bb4b4b7
guoxinl <lgx31@sina.cn>
2018-07-28 12:06:06 +0800

    ProtocolRegister add comment

--- communication-protocol-analysis/src/main/java/pub/guoxin/protocol/analysis/conf/register/ProtocolRegister.java
+++ communication-protocol-analysis/src/main/java/pub/guoxin/protocol/analysis/conf/register/ProtocolRegister.java
@@ -1,10 +1,18 @@
 package pub.guoxin.protocol.analysis.conf.register;
 
 /**
+ * 协议对象注册接口
+ * <p>
  * Created by guoxin on 18-3-8.
  */
 public interface ProtocolRegister<T> {
 
+    /**
+     * 注册协议对象
+     *
+     * @param element
+     * @return
+     */
     ProtocolRegister register(T element);
 
 }
