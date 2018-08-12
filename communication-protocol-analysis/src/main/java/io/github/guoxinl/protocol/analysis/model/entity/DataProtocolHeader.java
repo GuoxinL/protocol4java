c76bba89c433c730a951a0c470aa2d3af9292831
guoxinl <lgx31@sina.cn>
2018-08-13 01:33:34 +0800

    delete the meaningless DataProtocolIndexCode DataProtocolIndexCommand

--- communication-protocol-analysis/src/main/java/io/github/guoxinl/protocol/analysis/model/entity/DataProtocolHeader.java
+++ communication-protocol-analysis/src/main/java/io/github/guoxinl/protocol/analysis/model/entity/DataProtocolHeader.java
@@ -1,15 +1,12 @@
 package io.github.guoxinl.protocol.analysis.model.entity;
 
+import io.github.guoxinl.protocol.analysis.model.anno.Protocol;
 import io.netty.buffer.ByteBuf;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import io.github.guoxinl.protocol.analysis.model.anno.Protocol;
-
-import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * 协议：协议头
@@ -23,8 +20,8 @@ import java.util.Objects;
 @NoArgsConstructor
 public class DataProtocolHeader implements ProtocolSerialization {
 
-    private DataProtocolIndexCommand command;
-    private int                      version;
+    private int commandIndex;
+    private int version;
 
     /**
      * 将字节流转换为协议对象
@@ -34,37 +31,37 @@ public class DataProtocolHeader implements ProtocolSerialization {
     DataProtocolHeader(ByteBuf byteBuf) {
         {
             int commandIndex = byteBuf.readUnsignedByte();
-            log.debug("command readerIndex:{}",byteBuf.readerIndex());
-            this.command = DataProtocolIndexCommand.create(commandIndex);
+            log.debug("commandIndex readerIndex:{}", byteBuf.readerIndex());
+            this.commandIndex = commandIndex;
         }
         {
             this.version = byteBuf.readUnsignedByte();
-            log.debug("version readerIndex:{}",byteBuf.readerIndex());
+            log.debug("version readerIndex:{}", byteBuf.readerIndex());
         }
 
     }
 
     DataProtocolHeader(Protocol protocol) {
-        this.command = DataProtocolIndexCommand.create(protocol.commandIndex(), protocol.description());
+        this.commandIndex = protocol.commandIndex();
         this.version = protocol.version();
     }
 
-    public String getProtocolKey(){
-        return this.getCommand().getIndex() + "-" + this.version;
+    public String getProtocolKey() {
+        return this.commandIndex + "-" + this.version;
     }
 
     /**
      * 将协议对象序列化为字节流
      *
-     * @return 字节流
      * @param byteBuf
+     * @return 字节流
      */
     @Override
     public void serialization(ByteBuf byteBuf) {
         {
             // 协议 命令
-            byteBuf.writeByte(this.command.getIndex());
-            log.debug("command writerIndex:{}", byteBuf.writerIndex());
+            byteBuf.writeByte(this.commandIndex);
+            log.debug("commandIndex writerIndex:{}", byteBuf.writerIndex());
         }
         {
             // 协议 版本
